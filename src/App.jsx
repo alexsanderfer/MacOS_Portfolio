399c6b710c23d04696d985a91f859f0c63929127
Alexsander Fernandes <afgouveia90@gmail.com>
2025-11-21 21:47:35 +0000

    Add Welcome Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from "#components/Navbar.jsx";
+import {Navbar, Welcome} from "#components/index.js";
 
 function App() {
     return (
         <main>
             <Navbar/>
+            <Welcome/>
         </main>
     )
 }
